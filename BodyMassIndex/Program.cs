95213628cdacb615ffcdaec3f08195012e109321
Ali <alitaghizada2@gmail.com>
2023-01-03 10:50:54 +0400

    first commit

---
+++ BodyMassIndex/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BodyMassIndex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Добро пожаловать. Это программа позволит вам узнать индекс массы тела." +
+                "Для начала введите вес тела и рост.");
+
+            Console.Write("\nВес тела: ");
+            int bodyMass = int.Parse(Console.ReadLine());
+
+            Console.Write("\nРост: ");
+            int height = int.Parse(Console.ReadLine());
+
+            int bodyMassIndex = bodyMass / height * height;
+
+            if (bodyMassIndex <= 18)
+            {
+                Console.WriteLine("\nНиже нормального веса.");
+            }
+
+            else if (bodyMassIndex > 18 && bodyMassIndex < 25)
+            {
+                Console.WriteLine("\nНормальный вес.");
+            }
+
+            else if (bodyMassIndex > 25)
+            {
+                Console.WriteLine("\nОжирение.");
+            }
+
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Проверьте правильность ввода.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
